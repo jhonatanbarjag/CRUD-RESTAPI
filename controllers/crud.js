7a205b4a14e6fc0fa04ad6c2b6300e0eba71f62f
jhonatanbarjag <jhonatanbarja.g@hotmail.com>
2025-03-06 01:46:18 -0500

    Realizando crear editar y eliminar

--- controllers/crud.js
+++ controllers/crud.js
@@ -4,8 +4,8 @@ export async function save (req, res) {
     const user = req.body.user;
     const rol = req.body.rol;
 
-    // logica para guardar en la base de datos
-    conexion.query('insert into users set ?',{user:user, rol:rol}, (error, results) => {
+// logica para guardar en la base de datos
+conexion.query('insert into users set ?',{user:user, rol:rol}, (error, results) => {
         if (error) {
             console.log(error);
             
@@ -14,4 +14,23 @@ export async function save (req, res) {
         }
     });
 };
+
+//funcion para editar usuarios
+export async function update (req, res) {
+    const id = req.params.id;
+    const user = req.body.user;
+    const rol = req.body.rol;
+
+    //logica para actualizar en la base de datos
+    conexion.query ('update users set ? where id = ?', [{user:user, rol:rol}, id], (error, results) => {
+        if (error) {
+            console.log(error);
+            
+        } else {
+            console.log("actualizacion exitosa " , results);
+            res.redirect('/');
+        }
+    });
+
+}   
 
--- router/router.js
+++ router/router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { conexion } from "../database/db.js";
 import { save } from "../controllers/crud.js";
+import { update } from "../controllers/crud.js";
 
 const router = Router();
 
@@ -25,7 +26,40 @@ router.get('/create', (req, res) => {
     
     res.render('create');
 });
+//ruta para guardar usuarios
 router.post('/save', save);
+
+//ruta para editar usuarios
+router.get('/edit/:id', (req, res) => {
+    const id = req.params.id;
+    conexion.query('select * from users where id = ?', [id], (error, results) => {
+        if (error) {
+            throw error;
+            
+        } else {
+            res.render('edit', {user:results[0]});
+        }
+    });
+    
+});
+//ruta para actualizar usuarios
+router.post('/update/:id', update);
+
+
+//ruta para eliminar usuarios
+router.get('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    conexion.query('delete from users where id = ?', [id], (error, results) => {
+        if (error) {
+            throw error;
+            
+        } else {
+            console.log('Usuario eliminado');
+            res.redirect('/');
+        }
+    });
+}
+);
 
 
 
